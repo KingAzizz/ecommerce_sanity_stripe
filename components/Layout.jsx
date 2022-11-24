1d0a1a6f580d7a067e567fac30fc31a4f14f5bee
Abdulaziz <azozb997@gmail.com>
2022-11-24 18:17:55 +0300

    github corner

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import GithubCorner from 'react-github-corner';
+
 const Layout = ({children}) => {
   return (
     <div className='layout'>
@@ -9,6 +11,7 @@ const Layout = ({children}) => {
         <title>Alsunaydi Store</title>
       </Head> 
       <header>
+      <GithubCorner href='https://github.com/KingAzizz/ecommerce_sanity_stripe' direction='left' />
         <Navbar />
       </header>
       <main className='main-container'>
